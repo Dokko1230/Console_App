20066b3f57746de8a3f791bef015ae0985bd4d74
Sean Dokko <dokko1230@gmail.com>
2012-09-28 22:20:48 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+jQuery(function($, undefined) {
+	$("#terminal").terminal(function(command, term) {
+		if(command === "chan -help"){
+			term.echo("available commands are: hello ");
+		} else if (command == 'chan -help') {
+
+		} else {
+			term.echo('');
+		}
+	}, { 
+		greetings: 'Greetings! My name is Chan Dokko. It is a pleasure to meet you! Simply use this like a command line interface. Type \'sean -help\' for command line options.',
+		name: 'js_demo',
+		height: 700,
+		prompt: 'Chan>'});
+});
